bb9b0af2b5d8adba75645720c41ac7beb70bd70b
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-08-08 16:31:15 +0200

    add multi threaded executor to solve concurrent update of sim time

--- minimum_jerk_relative_move/minimum_jerk_relative_move/main.py
+++ minimum_jerk_relative_move/minimum_jerk_relative_move/main.py
@@ -1,12 +1,15 @@
 from .minimum_jerk_relative_move import MinimumJerkRelativeMove
 import rclpy
+from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimum_jerk_relative_move = MinimumJerkRelativeMove()
-
-    rclpy.spin(minimum_jerk_relative_move)
+ 
+    exe = MultiThreadedExecutor()
+    exe.add_node(MinimumJerkRelativeMove())
+    
+    while (rclpy.ok()):
+        exe.spin_once(timeout_sec=1.0)
 
 
 if __name__ == '__main__':
